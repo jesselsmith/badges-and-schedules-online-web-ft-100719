41fa30eb6c3aef9f240771a48b80249ad01690ad
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-07 11:36:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,20 @@ def batch_badge_creator(array)
     badge_maker(name)
   }
 end
+
+def assign_rooms(array)
+  room_assignments = []
+  array.each_with_index{ |name, index|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}"
+  }
+  room_assignments
+end
+
+def printer(array)
+  batch_badge_creator(array).each{ |badge|
+    puts badge
+  }
+  assign_rooms(array).each{ |assignment|
+    puts assignment
+  }
+end
